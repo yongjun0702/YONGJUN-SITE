b93bc3b8c3a1891d33d21a6793b8f18d25d489ca
yongjun0702 <joks0702@gachon.ac.kr>
2025-01-08 14:47:22 +0900

    [feat] footer 아이콘 배치 변경

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,34 +5,36 @@ import Button from "../Button/Button.jsx";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer-icons">
-                <a
-                    href="https://linkedin.com/in/yongjun0702"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="footer-icon linkedin"
-                >
-                    <FaLinkedin />
-                </a>
-                <a
-                    href="https://github.com/yongjun0702"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="footer-icon github"
-                >
-                    <FaGithub />
-                </a>
-                <a
-                    href="mailto:jo46453851@gmail.com"
-                    className="footer-icon email"
-                >
-                    <FaEnvelope />
-                </a>
+        <div className="footer">
+            <div className="footer-content">
+                <div className="footer-icons">
+                    <a
+                        href="https://linkedin.com/in/yongjun0702"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-icon linkedin"
+                    >
+                        <FaLinkedin />
+                    </a>
+                    <a
+                        href="https://github.com/yongjun0702"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-icon github"
+                    >
+                        <FaGithub />
+                    </a>
+                    <a
+                        href="mailto:jo46453851@gmail.com"
+                        className="footer-icon email"
+                    >
+                        <FaEnvelope />
+                    </a>
+                </div>
+                <div className="footer-text">2025 ⓒ YONGJUN. All rights reserved.</div>
+                <Button />
             </div>
-            <Button />
-            <p className="footer-text">2025 ⓒ YONGJUN. All rights reserved.</p>
-        </footer>
+        </div>
     );
 };
 
